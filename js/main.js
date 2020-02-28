91839cc19dcffa185601c1741576613f56e12f5c
Ritvik <ritvyk@gmail.com>
2020-02-28 23:36:43 +0530

    algo update

--- js/main.js
+++ js/main.js
@@ -5,39 +5,177 @@ function getCellId(string) {
     var arr = string.split("-");
     return parseInt(arr[1]);
 }
+function resetMovements()
+{
+    pieceAllowedMovements=[];
+}
 function getValidMoves() {
     return pieceAllowedMovements;
 }
 var rules = {
     pawn: function (cell) {
-        var blacksMove=10;
-        var step=0;
-        var whitesMove=-10;
-        if(getPlayerTurn()==="player1")
-        {
-            step=whitesMove;
+        var blacksMove = 10;
+        var step = 0;
+        var whitesMove = -10;
+        if (getPlayerTurn() === "player1") {
+            step = whitesMove;
         }
-        else{
-            step=blacksMove;
+        else {
+            step = blacksMove;
         }
         var cellid = getCellId(cell.id);
-        var nextRowCell = cellid +step;
+        var nextRowCell = cellid + step;
         var movesArray = [];
         movesArray.push(nextRowCell);
+        movesArray.push(cellid + (2 * step));
         movesArray.push(nextRowCell - 1);
         movesArray.push(nextRowCell + 1);
         pieceAllowedMovements = movesArray;
     },
     knight: function (cell) {
-
+        var blacksMove = 20;
+        var step = 0;
+        var stepBack = 0;
+        var whitesMove = -20;
+        if (getPlayerTurn() === "player1") {
+            step = whitesMove;
+            stepBack = 20
+        }
+        else {
+            step = blacksMove;
+            stepBack = -20;
+        }
+        var cellid = getCellId(cell.id);
+        var nextRowCell = cellid + step;
+        var movesArray = [];
+        //ahead
+        movesArray.push(nextRowCell + 1);
+        movesArray.push(nextRowCell - 1);
+        //left side for knight
+        movesArray.push((cellid + 2) - 10);
+        //right side for knight
+        movesArray.push((cellid - 2) - 10);
+        //downside
+        movesArray.push((cellid + stepBack) - 1);
+        movesArray.push((cellid + stepBack) + 1);
+        pieceAllowedMovements = movesArray;
     }, rook: function (cell) {
 
+        var cellid = getCellId(cell.id);
+        var movesArray = [];
+        var step=0;
+        // var stepback=10;
+        function rooksmove(direction,stepsAtATime)
+        {
+            if(direction==="f")
+            {
+                step=-10;
+            }
+            else if(direction==="b")
+            {
+                step=10;
+            }
+            else if(direction==="l")
+            {
+                step=-1;
+            }
+            else if(direction==="r")
+            {
+                step=1;
+            }
+            for(var i=0;i<=8;i++)
+            {
+                if(document.getElementById("cell-"+(cellid+step)))
+                {
+                   var nextCell=document.getElementById("cell-"+(cellid+step));
+                //    console.log(nextCell);
+                   if(nextCell.children.length!=0)
+                   {
+                        movesArray.push(cellid+step);
+                        break;
+                   }
+                   else{
+                    movesArray.push(cellid+step);
+                   }
+                }
+                step-=stepsAtATime;
+            }
+        }
+       rooksmove("f",10);
+       rooksmove("b",-10);
+       rooksmove("l",1);
+       rooksmove("r",1);
+        pieceAllowedMovements=movesArray;
     }, bishop: function (cell) {
-
+        var cellid = getCellId(cell.id);
+        var movesArray = [];
+        var step=0;
+        function bishopsMove(direction,stepsAtATime,diag)
+        {
+            if(direction==="fr")
+            {
+                step=-10;
+            }
+            else if(direction==="br")
+            {
+                step=10;
+            }
+            else if(direction==="fl")
+            {
+                step=-10;
+            }
+            else if(direction==="bl")
+            {
+                step=10;
+            }
+            for(var i=0;i<=8;i++)
+            {
+                if(document.getElementById("cell-"+(cellid+step+diag)))
+                {
+                   var nextCell=document.getElementById("cell-"+(cellid+step+diag));
+                //    console.log(nextCell);
+                   if(nextCell.children.length!=0)
+                   {
+                        console.log(nextCell);
+                        movesArray.push(cellid+step+diag);
+                        break;
+                   }
+                   else{
+                    console.log(nextCell);
+                    movesArray.push(cellid+step+diag);
+                   }
+                }
+                step-=stepsAtATime;
+                if(direction.includes("r")){
+                    diag-=1;
+                }
+                else if(direction.includes("l"))
+                {
+                    diag+=1;
+                }
+            }
+        }
+        //movinf....
+        bishopsMove("fr",10,-1);
+        bishopsMove("fl",10,1);
+        bishopsMove("br",-10,-1);
+        bishopsMove("bl",-10,1);
+        pieceAllowedMovements=movesArray;
+        
     }, queen: function (cell) {
 
     }, king: function (cell) {
-
+        var cellid=getCellId(cell.id);
+        var movesArray=[];
+        movesArray.push(cellid+10);
+        movesArray.push(cellid-10);
+        movesArray.push((cellid+10)+1);
+        movesArray.push((cellid+10)-1);
+        movesArray.push((cellid-10)-1);
+        movesArray.push((cellid-10)+1);
+        movesArray.push(cellid-1);
+        movesArray.push(cellid+1);
+        pieceAllowedMovements=movesArray;
     }
 }
 var playerStack = {
@@ -131,24 +269,23 @@ class ChessPiece {
         control.removePiece(moves.from, chessPiece);
         control.addPiece(moves.to, chessPiece);
         setPlayerStack("moved", true);
+        resetMovements();
         document.getElementById("step").play();
     }
     static seeIfMove(block) {
         var stack = getPlayerStack();
         if (stack.from != "") {
             var stack = getPlayerStack();
-            var validMove=false;
+            var validMove = false;
             if (block.children.length === 0) {
                 setPlayerStack("to", block.id);
-                var moves=getValidMoves();
+                var moves = getValidMoves();
                 moves.forEach(cellid => {
-                    if("cell-"+cellid===block.id)
-                    {
-                        validMove=true;
+                    if ("cell-" + cellid === block.id) {
+                        validMove = true;
                     }
                 });
-                if(validMove)
-                {
+                if (validMove) {
                     this.moveChessPiece();
                 }
             }
@@ -157,15 +294,13 @@ class ChessPiece {
                 var playerPieceInBlock = $(block.children[0]).data("player");
                 if (playerPieceInBlock != getPlayerTurn()) {
                     setPlayerStack("to", block.id);
-                    var moves=getValidMoves();
+                    var moves = getValidMoves();
                     moves.forEach(cellid => {
-                        if("cell-"+cellid===block.id)
-                        {
-                            validMove=true;
+                        if ("cell-" + cellid === block.id) {
+                            validMove = true;
                         }
                     });
-                    if(validMove)
-                    {
+                    if (validMove) {
                         this.moveChessPiece();
                     }
                 }
@@ -254,6 +389,7 @@ function startNewMove(e) {
             if (player === getPlayerTurn()) {
                 // console.log("matjced")
                 var pieceName = chessPiece.alt;
+                // console.log(pieceName)
                 if (pieceName === "pawn") {
                     rules.pawn(chessBlock);
                 }
@@ -269,7 +405,7 @@ function startNewMove(e) {
                     rules.king(chessBlock);
                 }
                 selectBlock(chessBlock);
-                // console.log(pieceAllowedMovements);
+                console.log(pieceAllowedMovements);
             }
         }
         else if (selected === "yes") {
